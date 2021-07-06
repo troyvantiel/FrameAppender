a96e7906c8526266d0a4115089afebd51c2ab97b
troyvantiel <troyvantiel@gmail.com>
2021-07-06 19:34:24 +1200

    fixes

--- FileProcess.java
+++ FileProcess.java
@@ -103,11 +103,6 @@ public class FileProcess
 
 
 			}
-			if(StandardDeviation < 0)
-			{
-			    System.out.println("Negative fixed");
-			    StandardDeviation = StandardDeviation * -1;
-            }
 			Std.add((float) Math.sqrt(StandardDeviation/ValArray.get(i).size()));
 		}
 		return Std;
